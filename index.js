3e2dc1f7a46dd6cd384c2356d9925c3552eb7f8d
Ben Paquin <bpaquin9@gmail.com>
2022-01-09 14:24:54 -0500

    built the api

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const pa11y = require("pa11y");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 const app = express();
 
-app.get("/api/test", (req, res) => {});
+app.use(express.static("public"));
+
+app.get("/api/test", async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: "url is required" });
+  } else {
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
